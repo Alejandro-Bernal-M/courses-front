05d9c597fcb8b9f7250dc09224f5e803fb92b99f
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-15 20:44:12 -0500

    update userSlice

--- redux/slices/userSlice.ts
+++ redux/slices/userSlice.ts
@@ -10,7 +10,21 @@ const initialState = {
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {}
+  reducers: {
+    signin: (state, action) => {
+      state.name = action.payload.user.name;
+      state.email = action.payload.user.email;
+      state.token = action.payload.token;
+      state.isLogged = true;
+    },
+    logout: (state) => {
+      state.name = '';
+      state.email = '';
+      state.token = '';
+      state.isLogged = false;
+    },
+  }
 })
 
+export const { signin, logout } = userSlice.actions;
 export default userSlice.reducer;
