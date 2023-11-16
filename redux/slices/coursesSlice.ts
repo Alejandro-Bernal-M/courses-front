5505313d8ff2b77a93d7bd5cab7fab6ffd6af40a
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-16 18:19:05 -0500

    Create async thunk for course details

--- redux/slices/coursesSlice.ts
+++ redux/slices/coursesSlice.ts
@@ -3,10 +3,19 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import apiEndpoints from '@/utils/apiEndpoints';
 import type { CourseProps } from '@/types/course';
+import toast from 'react-hot-toast';
 
 const initialState = {
   courses: [] as CourseProps[],
-  course: {},
+  course: {
+    _id: 'fetching data',
+    name: '',
+    description: '',
+    thumbnail: '',
+    enrollmentStatus: '',
+    prerequisites: ['fetching data'],
+
+  } as CourseProps,
   loading: false,
   error: false,
 }
@@ -23,6 +32,18 @@ export const getCourses = createAsyncThunk(
   }
 );
 
+export const getSpecificCourse = createAsyncThunk(
+  'courses/getSpecificCourse',
+  async(id: string) => {
+    try {
+      const response = await axios.get(`${apiEndpoints.specificCourse(id)}`);
+      return response.data;
+    } catch (error) {
+      console.log('error', error)
+      toast.error('Error fetching course');
+    }
+  }
+);
 
 const coursesSlice = createSlice({
   name: 'courses',
@@ -40,6 +61,18 @@ const coursesSlice = createSlice({
       state.loading = false;
       state.error = true;
       console.error('Error fetching courses:', action.error);
+    });
+    builder.addCase(getSpecificCourse.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(getSpecificCourse.fulfilled, (state, action) => {
+      state.loading = false;
+      state.course = action.payload;
+    });
+    builder.addCase(getSpecificCourse.rejected, (state, action) => {
+      state.loading = false;
+      state.error = true;
+      console.error('Error fetching course:', action.error);
     });
   }
 });
