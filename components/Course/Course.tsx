19b16fcc46042d688b167d197b10cd797d187e88
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-16 16:24:09 -0500

    Add styles to course component

--- components/Course/Course.tsx
+++ components/Course/Course.tsx
@@ -1,4 +1,5 @@
 import type { CourseProps } from '@/types/course'
+import styles from './course.module.css'
 
 function Course({
     _id,
@@ -15,19 +16,30 @@ function Course({
   }: CourseProps) {
 
   return (
-    <div>
-      <h1>{name}</h1>
-      {description}
-      {duration}
-      {enrollmentStatus}
-      {instructor}
-      {location}
-      {prerequisites.map((prerequisite) => (
-        <p>{prerequisite}</p>
-      ))}
-      {schedule}
-
-      <img src={`${process.env.NEXT_PUBLIC_API}/public/${thumbnail}`} alt="" />
+    <div className={styles.courseHolder}>
+      <h2 className={styles.courseTitle}>{name}</h2>
+      <div className={styles.infoHolder}>
+        <div className={styles.imgHolder}>
+          <img src={`${process.env.NEXT_PUBLIC_API}/public/${thumbnail}`} alt="" />
+        </div>
+        <div className={styles.courseContent}>
+          <p className={styles.description}>{description}</p>
+          <p> Duration: {duration}</p>
+          <p> Status: {enrollmentStatus}</p>
+          <p> Instructor:{instructor}</p>
+          <p> Location: {location}</p>
+          <p> Schedule: {schedule}</p>
+          <p>Prerequisites:</p>
+          <ul className={styles.prerequisitesUl}>
+            {prerequisites.map((prerequisite) => (
+              <li className={styles.prerequisitesLi}>{prerequisite}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className={styles.buttonsHolder}>
+        <button className='button' >View Details</button>
+      </div>
     </div>
   )
 }
