9f170701eccdb7d224d6b67027220355b9108478
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-16 09:23:24 -0500

    Add asyncThunk to retrieve the courses

--- redux/slices/coursesSlice.ts
+++ redux/slices/coursesSlice.ts
@@ -1,16 +1,47 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import apiEndpoints from '@/utils/apiEndpoints';
+import type { CourseProps } from '@/types/course';
 
 const initialState = {
-  courses: [],
+  courses: [] as CourseProps[],
   course: {},
   loading: false,
-  error: null,
+  error: false,
 }
+
+export const getCourses = createAsyncThunk(
+  'courses/getCourses',
+  async() => {
+    try {
+      const response = await axios.get(apiEndpoints.courses);
+      return response.data;
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+);
+
 
 const coursesSlice = createSlice({
   name: 'courses',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCourses.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(getCourses.fulfilled, (state, action) => {
+      state.loading = false;
+      state.courses = action.payload.filteredCourses;
+    });
+    builder.addCase(getCourses.rejected, (state, action) => {
+      state.loading = false;
+      state.error = true;
+      console.error('Error fetching courses:', action.error);
+    });
+  }
 });
 
 export default coursesSlice.reducer;
