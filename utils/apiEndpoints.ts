d84490b52fc257ad6ab95ecf09e9524e191a994e
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-16 18:18:34 -0500

    Add API endpoint

--- utils/apiEndpoints.ts
+++ utils/apiEndpoints.ts
@@ -2,6 +2,7 @@ const apiEndpoints = {
   signup: process.env.NEXT_PUBLIC_API+'/api/signup',
   signin: process.env.NEXT_PUBLIC_API+'/api/signin',
   courses: process.env.NEXT_PUBLIC_API+'/api/courses',
+  specificCourse: (id:string) => process.env.NEXT_PUBLIC_API+`/api/courses/${id}`,
 };
 
 export default apiEndpoints;
