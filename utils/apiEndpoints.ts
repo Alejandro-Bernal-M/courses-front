c55f74186cd1edd21d2ff3f7b44dd4ccd9bb7f29
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-16 09:24:04 -0500

    Add api endpoint to fetch courses and update the others endpoints

--- utils/apiEndpoints.ts
+++ utils/apiEndpoints.ts
@@ -1,6 +1,7 @@
 const apiEndpoints = {
-  signup: process.env.NEXT_PUBLIC_API+'/signup',
-  signin: process.env.NEXT_PUBLIC_API+'/signin',
+  signup: process.env.NEXT_PUBLIC_API+'/api/signup',
+  signin: process.env.NEXT_PUBLIC_API+'/api/signin',
+  courses: process.env.NEXT_PUBLIC_API+'/api/courses',
 };
 
 export default apiEndpoints;
