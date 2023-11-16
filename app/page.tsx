65f6d8a3a2250581a8d1180870a5e06848ba080f
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-16 09:25:03 -0500

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      hello
+      <h1>Welcome to Get Knowledge</h1>
+      <p>The Knowledge awaits for you</p>
      </main>
   )
 }
