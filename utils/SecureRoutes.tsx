e80074316f58545a8c2ac6a434b82a4118f940a8
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-17 13:25:47 -0500

    update secure routes

--- utils/SecureRoutes.tsx
+++ utils/SecureRoutes.tsx
@@ -21,18 +21,26 @@ const RouteSecurity = ({ children }: {children: React.ReactNode}) => {
   const pathname = usePathname();
   const myStore = useSyncExternalStore(store.subscribe, store.getState, store.getState);
   const loggedInUser = myStore.user.isLogged;
+  const role = myStore.user.role;
    
   const permitedRoutesRegex = /^(\/$|\/signin$|\/signup$|\/courses(\/[a-zA-Z0-9]+)*$)/;
   const restrictedSignedInRoutes = ["/signin", "/signup"];
 
   useEffect(() => {
+    if(role !== 'admin' && pathname === '/admin'){
+      toast.error("You don't have permission to access this page");
+      push("/");
+      return;
+    }
     if ( !permitedRoutesRegex.test(pathname)  && !loggedInUser) {
       toast.error("Please login first");
       push("/");
+      return;
     }
     if (restrictedSignedInRoutes.includes(pathname) && loggedInUser) {
       toast.success("Logged In");
       push("/");
+      return;
     }
   }, [pathname, loggedInUser]);
 
