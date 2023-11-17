a14d109e569e6c5001c5c8b99bccc4e7c0c82433
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-16 20:02:54 -0500

    Add specificCourseSingin endpoint

--- utils/apiEndpoints.ts
+++ utils/apiEndpoints.ts
@@ -3,6 +3,7 @@ const apiEndpoints = {
   signin: process.env.NEXT_PUBLIC_API+'/api/signin',
   courses: process.env.NEXT_PUBLIC_API+'/api/courses',
   specificCourse: (id:string) => process.env.NEXT_PUBLIC_API+`/api/courses/${id}`,
+  specificCourseSignin: (userId:string, courseId:string ) => process.env.NEXT_PUBLIC_API+`/api/user/${userId}/courses/${courseId}`,
 };
 
 export default apiEndpoints;
