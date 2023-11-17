397913e0ee7976cd44e761522fd43c2f1700e3de
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-17 09:59:35 -0500

    Add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import styles from './page.module.css'
+import { useEffect, useSyncExternalStore } from 'react'
+import { useDispatch } from 'react-redux'
+import { getenrolledCourses } from '@/redux/slices/userSlice'
+import { ThunkDispatch } from '@reduxjs/toolkit'
+import store from '@/redux/store'
+import EnrolledCourse from '@/components/EnrolledCourse/EnrolledCourse'
+
+function Dashboard() {
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
+  const myStore = useSyncExternalStore(store.subscribe, store.getState, store.getState);
+  const userStore = myStore.user;
+  const enrolledCourses = userStore.enrolledCourses;
+
+  useEffect(() => {
+    dispatch(getenrolledCourses(userStore.token));
+  }, [dispatch]);
+
+  console.log(enrolledCourses);
+
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.title}>Enrolled Courses</h1>
+      <div className={styles.coursesHolder}>
+        {enrolledCourses.map((course: any) => (
+          <EnrolledCourse key={course.id} course={course} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Dashboard
